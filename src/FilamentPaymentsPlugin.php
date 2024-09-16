42b2f8d992ff6ed65b714e2c5baf005791d274d4
Fady Mondy <info@3x1.io>
2024-09-16 18:35:07 +0300

    add filament-plugins integration

--- src/FilamentPaymentsPlugin.php
+++ src/FilamentPaymentsPlugin.php
@@ -4,12 +4,15 @@ namespace TomatoPHP\FilamentPayments;
 
 use Filament\Contracts\Plugin;
 use Filament\Panel;
+use Nwidart\Modules\Module;
 use TomatoPHP\FilamentPayments\Filament\Pages\PaymentGateway;
 use TomatoPHP\FilamentPayments\Filament\Resources\PaymentGatewayResource;
 use TomatoPHP\FilamentPayments\Filament\Resources\PaymentResource;
 
 class FilamentPaymentsPlugin implements Plugin
 {
+    private bool $isActive = false;
+
     public function getId(): string
     {
         return 'filament-payments';
@@ -17,13 +20,24 @@ class FilamentPaymentsPlugin implements Plugin
 
     public function register(Panel $panel): void
     {
-        $panel
-            ->pages([
-                PaymentGateway::class
-            ])
-            ->resources([
-                PaymentResource::class
-            ]);
+        if(class_exists(Module::class)){
+            if(\Nwidart\Modules\Facades\Module::find('FilamentPayments')->isEnabled()){
+                $this->isActive = true;
+            }
+        }
+        else {
+            $this->isActive = true;
+        }
+
+        if($this->isActive) {
+            $panel
+                ->pages([
+                    PaymentGateway::class
+                ])
+                ->resources([
+                    PaymentResource::class
+                ]);
+        }
     }
 
     public function boot(Panel $panel): void
