b7ed274d3521981eaf15e4a171b5da42bd2b01c7
Abdelmjid Saber <87904671+megoxv@users.noreply.github.com>
2024-09-17 18:55:24 +0300

    fix ->isEnabled()

--- src/FilamentPaymentsPlugin.php
+++ src/FilamentPaymentsPlugin.php
@@ -20,10 +20,8 @@ class FilamentPaymentsPlugin implements Plugin
 
     public function register(Panel $panel): void
     {
-        if(class_exists(Module::class)){
-            if(\Nwidart\Modules\Facades\Module::find('FilamentPayments')?->isEnabled()){
-                $this->isActive = true;
-            }
+        if(class_exists(Module::class) && \Nwidart\Modules\Facades\Module::find('FilamentPayments')?->isEnabled()){
+            $this->isActive = true;
         }
         else {
             $this->isActive = true;
